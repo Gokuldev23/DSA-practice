c8248aebb8657f16b0bae425aada55f803cb66de
Gokuldev23 <gkforgeneral@gamil.com>
2025-07-13 10:43:18 +0530

    Finding GCD using Euclidean algorithm

---
+++ Math/gcd.ts
@@ -0,0 +1,30 @@
+/**
+ * ✅ Solved on 2025-07-12
+ * Leetcode #438
+ * Approach: Ecludien Algorithm
+ * Time: O(n * m), Space: O(1)
+ * Where n = s.length, m = p.length
+ */
+
+
+const gcd = (a:number,b:number) => {
+    if(a == 0) return b
+    return gcd(a%b,a)
+}
+
+
+const findDcdinArr = (arr:number[]) => {
+
+    let res = arr[0]
+    for(let i=1;i<arr.length;i++) {
+        res = gcd(res,arr[i])
+    }
+    return res
+}
+
+let a = 80
+let b = 64
+let arr = [20,44,78,90,124,190]
+
+console.log(gcd(a,b))
+console.log("GCD arr",findDcdinArr(arr));

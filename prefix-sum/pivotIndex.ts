bb6229bb41391e0b1cc37d840791a6875f8c11c6
Gokuldev23 <gkforgeneral@gamil.com>
2025-07-14 10:06:02 +0530

    Prefix sum algorithm easy level problem - PivotIndex solved

---
+++ prefix-sum/pivotIndex.ts
@@ -0,0 +1,27 @@
+/**
+ * ✅ Solved on 2025-07-14
+ * Leetcode #438
+ * Approach: Ecludien Algorithm
+ * Time: O(n), Space: O(1)
+ * Where n = s.length, m = p.length
+ */
+
+
+const pivotIndex = (nums: number[]) => {
+
+    let totalSum = nums.reduce((prev,curr)=>prev+=curr,0)
+    
+    let leftSum = 0
+    for(let i=0;i<nums.length;i++) {
+        let rightSum = totalSum - leftSum - nums[i]
+        if(leftSum === rightSum) {
+            return i
+        }
+        leftSum+= nums[i]
+    }
+    return -1
+}
+
+let nums = [1, 7, 3, 6, 5, 6]
+
+console.log(pivotIndex(nums))

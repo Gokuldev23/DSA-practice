957efc3725753f85170d4337e8ff8e47f2457439
Gokuldev23 <gkforgeneral@gmail.com>
2025-07-15 21:32:40 +0530

    tried : subArraySum with O(n) algorithm way tried but now solved yet

--- prefix-sum/subArraySum.ts
+++ prefix-sum/subArraySum.ts
@@ -1,5 +1,5 @@
 
-
+//Brute force
 const subArraySum = (array:number[],k:number) => {
 
     let count = 0
@@ -19,5 +19,30 @@ const subArraySum = (array:number[],k:number) => {
     return count
 }
 
-console.log(subArraySum([1,1,1],2))
-console.log(subArraySum([1,7,4,3,5,6],7))
+// console.log(subArraySum([1,1,1],2))
+// console.log(subArraySum([1,7,4,3,5,6],7))
+
+const subArraySumOptimzed  = (array:number[],k:number) => {
+
+    let map = {0:1}
+    let count = 0
+
+    let prefixArr = array.reduce((accum: number[], val, i) => {
+        accum.push(val + (accum[i - 1] ?? 0));
+        return accum;
+    }, []);
+
+    for (let i = 1; i < array.length; i++) {
+        
+        let prefixSum = prefixArr[i-1] - k
+        if(map[prefixSum]){
+            count++
+        }
+        map[i] = prefixSum
+    }
+
+    return count
+
+}
+
+console.log(subArraySumOptimzed([1,7,4,3,5,6],7));

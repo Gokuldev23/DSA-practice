d764358d79f791ebdd9a2905890ebfc627ff51f3
Gokuldev23 <gkforgeneral@gmail.com>
2025-07-17 17:42:18 +0530

    solved:top k frequent element problem solved

---
+++ hashTables/topKFreqElem.ts
@@ -0,0 +1,30 @@
+
+
+
+function topKFrequent(nums: number[], k: number): number[] {
+
+    const hashMap = new Map<number, number>()
+
+    const arr: number[][] = Array.from({length:nums.length + 1},()=>[])
+
+    for(const n of nums){
+        hashMap.set(n,( hashMap.get(n) || 0 ) + 1)
+    }
+
+    hashMap.forEach((value: number, key: number)=> {
+        arr[value].push(key)
+    })
+
+    let res: number[] = []
+
+    for(let i=arr.length-1;i>0;i--){
+        for(const n of arr[i]){
+            res.push(n)
+            if(res.length == k) return res
+        }
+    }
+
+    console.log({arr})
+    return res
+}
+console.log(topKFrequent( [1,2,2,3,3,3],2))
